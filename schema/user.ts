fee52f4581cca3024cd7f73d81eeb7d72415fb73
Bryan Gabriel <bryangabrielberja25@gmail.com>
2024-03-14 16:51:10 +0800

    add_LoginSchema

--- schema/user.ts
+++ schema/user.ts
@@ -4,4 +4,9 @@ export const SignupSchema = z.object({
     name     : z.string().min(1,'Name is required'),
     email    : z.string().email().min(1, 'Email is required'),
     password : z.string().min(8, 'Password atleast 8 character').max(12)
+});
+
+export const LoginSchema = z.object({
+    email    : z.string().email().min(1, 'Email is required'),
+    password : z.string()
 })
