4f72c7b0d4729571d9f69cc0ca673c9cbae5456b
Bryan Gabriel <bryangabrielberja25@gmail.com>
2024-03-14 14:26:16 +0800

    Add_errorHandler_for_authController

--- routes/auth.ts
+++ routes/auth.ts
@@ -1,8 +1,10 @@
-import express from 'express';
-import * as controller from '../controller/authController'
+import express          from 'express';
+import * as controller  from '../controller/authController'
+import { errorHandler } from '../middleware/error-handler';
+
 const authRouter = express.Router()
 
-authRouter.post('/signup', controller.signup)
-authRouter.post('/login', controller.login)
+authRouter.post('/signup', errorHandler(controller.signup))
+authRouter.post('/login', errorHandler(controller.login))
 
 export default authRouter
