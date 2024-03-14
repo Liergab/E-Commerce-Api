d900124aaf6f2f8ae569841b85dd996e35a897bc
Bryan Gabriel <bryangabrielberja25@gmail.com>
2024-03-14 14:28:15 +0800

    add_error_validation_and_Schema

--- controller/authController.ts
+++ controller/authController.ts
@@ -1,10 +1,15 @@
-import { Request,
-         Response }            from 'express';
-import { prismaClient }        from '..';
+import { NextFunction, Request,
+         Response }                    from 'express';
+import { prismaClient }                from '..';
 import { hashpassword, 
-         passwordCompared }    from '../config/bcrypt';
-import   jwt                   from 'jsonwebtoken'
-import { JWT_SECRET } from '../secret';
+         passwordCompared }            from '../config/bcrypt';
+import { generateToken }               from '../middleware/generateToken';
+import { BadRequestsExeption }         from '../execptions/bad-request';
+import { ErrorCode }                   from '../execptions/root';
+import { UnautorizedRequestException } from '../execptions/unautorized-request';
+import { ConflictRequestsExeption }    from '../execptions/conflict-request';
+import { UnprocessableEntity }         from '../execptions/validation';
+import { SignupSchema }                from '../schema/user';
 
 /*
     path api/auth/signup
@@ -17,12 +22,14 @@ type SignupRequestBody = {
     password : string,
     name     : string
 }
-export const signup = async(req:Request<{}, {}, SignupRequestBody>,res:Response) => {
-    const {email, password, name} = req.body
-    try {
+export const signup = async(req:Request<{}, {}, SignupRequestBody>,res:Response, next:NextFunction) => {
+   
+        SignupSchema.parse(req.body)
+        const {email, password, name} = req.body
         const user = await prismaClient.user.findFirst({where:{email}});
         if(user){
-            res.status(400).json({error:'Email already Taken!'});
+            // res.status(400).json({error:'Email already Taken!'});
+            next(new ConflictRequestsExeption('Email already Taken!', ErrorCode.CONFLICT_REQUEST))
         }
             const newUser = await prismaClient.user.create({
                 data:{
@@ -35,13 +42,9 @@ export const signup = async(req:Request<{}, {}, SignupRequestBody>,res:Response)
         if(newUser){
             res.status(201).json({data:newUser});
         }else{
-            res.status(404).json({error:'User not Created!'})
+            next(new BadRequestsExeption('Failed to create Account!', ErrorCode.BAD_REQUEST))
         }
 
-    } catch (error) {
-        console.log(error);
-        res.status(500).json({error:error});
-    }
 }
 
 
@@ -55,22 +58,22 @@ type loginRequestBody = {
     email    : string;
     password : string;
 }
-export const login = async(req:Request<{},{}, loginRequestBody>, res:Response) => {
-    const{email, password} = req.body
+export const login = async(req:Request<{},{}, loginRequestBody>, res:Response, next:NextFunction) => {
+    const{email, password} = req.body;
     try {
         const user = await prismaClient.user.findFirst({where:{email}});
         if(user && await passwordCompared({plain:password, hashPassword:user.password })){
-           const token = jwt.sign({userId: user.id}, JWT_SECRET)
+            const userId = user.id;
             res.status(200).json({
                 id:user.id,
                 name:user.name,
                 email:user.email,
-                password:user.password,
                 createAt:user.createAt,
-                token:token
+                token:generateToken(userId)
             });
         }else{
-            res.status(400).json({error:'Invalid Credentials'})
+            
+            next(new UnautorizedRequestException('Invalid Credentials', ErrorCode.INCORRECT_CREDENTIALS))
         }
     } catch (error) {
         console.log(error);
