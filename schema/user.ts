eca4a68177dfcd6c40ffa70b43632ff0fda8936a
Bryan Gabriel <bryangabrielberja25@gmail.com>
2024-03-14 14:31:34 +0800

    zod_schema

---
+++ schema/user.ts
@@ -0,0 +1,7 @@
+import {z} from 'zod';
+
+export const SignupSchema = z.object({
+    name     : z.string().min(1,'Name is required'),
+    email    : z.string().email().min(1, 'Email is required'),
+    password : z.string().min(8, 'Password atleast 8 character').max(12)
+})
