7d3332b750b470303b26b3a8bc9e9e9556a08c10
Bryan Gabriel <bryangabrielberja25@gmail.com>
2024-03-15 18:47:19 +0800

    reusable_auth_types

---
+++ types/auth/types.ts
@@ -0,0 +1,10 @@
+export type SignupRequestBody = {
+    email    : string;
+    password : string,
+    name     : string
+}
+
+export type loginRequestBody = {
+    email    : string;
+    password : string;
+}
