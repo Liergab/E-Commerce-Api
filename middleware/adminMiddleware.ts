0d78349100492a03a496feec830c185739b27343
Bryan Gabriel <bryangabrielberja25@gmail.com>
2024-03-15 18:45:53 +0800

    role_authentication

---
+++ middleware/adminMiddleware.ts
@@ -0,0 +1,12 @@
+import { NextFunction, Response }      from "express";
+import { AuthenticatedRequest }        from "../types/express";
+import { UnautorizedRequestException } from "../execptions/authentication/unautorized-request";
+import { ErrorCode }                   from "../execptions/root";
+
+export const adminMiddleware = async(req:AuthenticatedRequest, res:Response, next:NextFunction) => {
+    if(req.user?.role == "ADMIN"){
+        next()
+    }else{
+        next(new UnautorizedRequestException('Unauthorized', ErrorCode.UNAUTHORIZED))
+    }
+}
