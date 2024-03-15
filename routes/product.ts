430b3a79d69ae9fe42d817c9fe72df8bd0608b59
Bryan Gabriel <bryangabrielberja25@gmail.com>
2024-03-15 18:47:53 +0800

    product_routes

---
+++ routes/product.ts
@@ -0,0 +1,13 @@
+import express             from 'express';
+import * as controller      from '../controller/productController'
+import { authMiddleware }   from '../middleware/authMiddleware';
+import { adminMiddleware }  from '../middleware/adminMiddleware';
+const productRoutes = express.Router();
+
+productRoutes.post('/',     [authMiddleware, adminMiddleware], controller.createProduct)
+productRoutes.patch('/:id', [authMiddleware, adminMiddleware], controller.updateProduct)
+productRoutes.delete('/:id',[authMiddleware, adminMiddleware], controller.deleteProduct)
+productRoutes.get('/:id',   [authMiddleware, adminMiddleware], controller.getProductById)
+productRoutes.get('/all',   [authMiddleware, adminMiddleware], controller.getAllProduct)
+
+export default productRoutes
