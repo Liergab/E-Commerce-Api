0e03d0b8dc1cc34bb8ee5217a6f5b4268ca7a179
Bryan Gabriel <bryangabrielberja25@gmail.com>
2024-03-15 18:44:02 +0800

    add_product_routes

--- routes/index.ts
+++ routes/index.ts
@@ -1,7 +1,10 @@
-import express from 'express'
-import authRouter from './auth';
+import express          from 'express'
+import authRouter       from './auth';
+import productRoutes    from './product';
+
 const rootRouter = express.Router();
 
 rootRouter.use('/auth', authRouter)
+rootRouter.use('/product', productRoutes)
 
 export default rootRouter
